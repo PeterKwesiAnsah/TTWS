11b158ef0cd0391234d6504fc12fba9c085a3385
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2026-05-25 22:43:34 +0000

    parse userid from URI

--- db.client.c
+++ db.client.c
@@ -1,13 +1,30 @@
 #include "rio.h"
 #include <postgresql/libpq-fe.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#define URI_ARG_INDEX 1
+#define METHOD_ARG_INDEX 2
+#define VERSION_ARG_INDEX 3
+
+void client_error(int connfd, int status, const char *msg) {
+  char res[MAXLINE];
+  sprintf(res, "HTTP/1.1 %d %s\r\n\r\n", status, msg);
+  rio_write(connfd, res, strlen(res));
+  close(connfd);
+}
 // Create a single open connection (using environmental variables corresponding
 // to the parameters ) to the database server (PgConn *)
 int main(int argc, char *argv[]) {
-  // fprintf(stderr, "Connecting to database\n");
+  if (argc != 4) {
+    fprintf(stderr, "Usage: %s <uri> <method> <version>\n", argv[0]);
+    client_error(STDOUT_FILENO, 400, "Bad Request");
+    exit(1);
+  }
+
   PGconn *dbconn =
       PQconnectdbParams((const char *[]){NULL}, (const char *[]){NULL}, 0);
   if (PQstatus(dbconn) != CONNECTION_OK) {
@@ -21,24 +38,30 @@ int main(int argc, char *argv[]) {
     sprintf(buf, "%sContent-Type: text/plain\r\n", buf);
     sprintf(buf, "%sContent-Length: %lu\r\n", buf, strlen(res));
     sprintf(buf, "%s\r\n", buf);
-    printf("%s%s", buf, res);
-    fflush(stdout);
+
+    rio_write(STDOUT_FILENO, buf, strlen(buf));
+    rio_write(STDOUT_FILENO, res, strlen(res));
     close(STDOUT_FILENO);
     PQfinish(dbconn);
     return 1;
   }
 
-  char buf[MAXLINE];
-  char *res = "Connection to database successful\r\n";
-  // response line
-  sprintf(buf, "HTTP/1.1 200 OK\r\n");
-  // response headers
-  sprintf(buf, "%sContent-Type: text/plain\r\n", buf);
-  sprintf(buf, "%sContent-Length: %lu\r\n", buf, strlen(res));
-  // end of headers
-  sprintf(buf, "%s\r\n", buf);
-  rio_write(STDOUT_FILENO, buf, strlen(buf));
-  rio_write(STDOUT_FILENO, res, strlen(res));
+  // uri between /users/<userid> and /
+  // users table have id | first_name | last_name columns
+  if (argv[URI_ARG_INDEX] && strlen(argv[URI_ARG_INDEX]) == 1) {
+    // /
+  } else {
+    // /users/<user-id
+    char *suser_id = argv[URI_ARG_INDEX] + strlen("/users/");
+    char *endptr;
+    // check for non-valid string
+    size_t user_id = strtol(suser_id, &endptr, 10);
+    if (*endptr != '\0' || suser_id == endptr) {
+      client_error(STDOUT_FILENO, 400, "Bad Request");
+      PQfinish(dbconn);
+      exit(1);
+    }
+  }
   close(STDOUT_FILENO);
   PQfinish(dbconn);
   return 0;
